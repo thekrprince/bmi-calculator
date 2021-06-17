8e5838104447a2512374df651950c77109e37064
princekr96 <princekr700@gmail.com>
2021-06-17 23:37:48 +0530

    Done

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 const heightInput = document.querySelector('#height');
 const weightInput = document.querySelector('#weight');
+const result = document.querySelector('#result');
+const message = document.querySelector('#message');
+const calculate = document.querySelector('#calculate');
 
-let height, weight;
+let height, weight, output, msg;
 
 // Height event listener
 heightInput.addEventListener('change', (e) => {
   checkNumber(e.target.value);
-
   height = e.target.value;
-  console.log(height);
 });
 
 // Weight event listener
@@ -16,6 +17,30 @@ weightInput.addEventListener('change', (e) => {
   checkNumber(e.target.value);
   weight = e.target.value;
 });
+
+calculate.addEventListener('click', () => {
+  calculateBMI();
+  category();
+});
+
+// Calculate BMI function
+const calculateBMI = () => {
+  output = weight / (height / 100) ** 2;
+  result.textContent = output.toString().slice(0, 4);
+};
+
+const category = () => {
+  if (output < 18.5) {
+    msg = 'Underweight';
+  } else if (output >= 18.5 && output < 25) {
+    msg = 'Normal';
+  } else if (output >= 25 && output < 30) {
+    msg = 'Overweight';
+  } else if (output >= 30) {
+    msg = 'Obese';
+  }
+  message.innerHTML = msg;
+};
 
 // Input Validation
 const checkNumber = (val) => {
